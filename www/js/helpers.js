5415616f9066666c290cfe120a8297f962becffc
TonSuchat <suchat.v26@gmail.com>
2016-04-01 15:01:01 +0700

    เพิ่ม function Generated GUID

--- www/js/helpers.js
+++ www/js/helpers.js
@@ -93,3 +93,13 @@ function TransformServerTSToDateTimeStr(ts){
     if(ts.length == 13) return result = ts.substring(0,1) + '/' + ts.substring(1,3) + '/' + ts.substring(3,7) + ' ' + ts.substring(7,9) + ':' + ts.substring(9,11);
     else return result = ts.substring(0,2) + '/' + ts.substring(2,4) + '/' + ts.substring(4,8) + ' ' + ts.substring(8,10) + ':' + ts.substring(10,12);
 };
+
+function GeneratedGUID(){
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+          .toString(16)
+          .substring(1);
+    }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+};
