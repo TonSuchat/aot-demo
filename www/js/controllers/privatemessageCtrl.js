4ce630d0365ca5ef01589000873e318f5e055af7
TonSuchat <suchat.v26@gmail.com>
2016-04-12 13:50:58 +0700

    Implement have/haven't internet connection

--- www/js/controllers/privatemessageCtrl.js
+++ www/js/controllers/privatemessageCtrl.js
@@ -3,33 +3,27 @@ angular.module('starter')
 .controller('PrivateMessageRoomsCtrl',function($scope,$ionicPlatform,SyncService,PMRoomSQLite,APIService,$cordovaNetwork,$ionicPopup){
     
     $ionicPlatform.ready(function(){
-
-        //if no internet connection
-        if(!CheckNetwork($cordovaNetwork)){
-             OpenIonicAlertPopup($ionicPopup,'ไม่มีสัญญานอินเตอร์เนท','ไม่สามารถใช้งานส่วนนี้ได้เนื่องจากไม่ได้เชื่อมต่ออินเตอร์เนท');
-        };
-          
         $scope.haveMoreData = false;
         $scope.isfirstLoad = true;
         $scope.roomsDetail = [];
         $scope.allRooms = [];
         $scope.scrollDetails = {start:1,retrieve:10};
-
         APIService.ShowLoading();
-        //sync rooms and insert/update room to sqlite
-        SyncService.SyncPMRoom().then(
-            function(){
-                //select all rooms from sqlite and bind to roomsDetail        
-                PMRoomSQLite.GetAll().then(
-                    function(response){
-                        if(response.rows != null) {
-                            $scope.allRooms = ConvertQueryResultToArray(response);
-                            $scope.roomsDetail = GetInfiniteScrollData($scope.allRooms,$scope.scrollDetails.start,$scope.scrollDetails.retrieve);
-                            $scope.haveMoreData = (($scope.scrollDetails.start + $scope.scrollDetails.retrieve) < $scope.allRooms.length) ? true : false;
-                            FinalAction($scope, APIService);
-                        }
-                },function(){FinalAction($scope, APIService);});
-        },function(){FinalAction($scope, APIService);});
+
+        //if no internet connection
+        if(!CheckNetwork($cordovaNetwork)){
+            OpenIonicAlertPopup($ionicPopup,'ไม่มีสัญญานอินเตอร์เนท','ไม่สามารถใช้งานส่วนนี้ได้เนื่องจากไม่ได้เชื่อมต่ออินเตอร์เนท');
+            //select all rooms from sqlite and bind to roomsDetail        
+            PMRoomInitialProcess($scope,PMRoomSQLite,APIService);
+        }
+        else{
+            //sync rooms and insert/update room to sqlite
+            SyncService.SyncPMRoom().then(
+                function(){
+                    //select all rooms from sqlite and bind to roomsDetail        
+                    PMRoomInitialProcess($scope,PMRoomSQLite,APIService);
+            },function(){FinalAction($scope, APIService);});
+        }
 
          //this function trigger by ng-infinite-scroll (when scrolled to bottom)
         $scope.loadMoreData = function () {
@@ -48,18 +42,6 @@ angular.module('starter')
 .controller('PrivateMessagesCtrl',function($scope,$ionicScrollDelegate,$stateParams,PMMsgSQLite,$filter,APIService,$ionicPlatform,SyncService,socketFactory,$rootScope,PMRoomSQLite,$ionicPopover,$cordovaNetwork,$ionicPopup,PMSubscribeSQLite){
     $ionicPlatform.ready(function(){
 
-        //popover private message menus
-        $ionicPopover.fromTemplateUrl('templates/PMMenus.html', {scope: $scope,}).then(function(popover) { $scope.popover = popover; });
-
-        $scope.noInternet = false;
-        //if no internet connection
-        if(!CheckNetwork($cordovaNetwork)){
-            $scope.noInternet = true;
-            OpenIonicAlertPopup($ionicPopup,'ไม่มีสัญญานอินเตอร์เนท','ไม่สามารถใช้งานส่วนนี้ได้เนื่องจากไม่ได้เชื่อมต่ออินเตอร์เนท');
-        };
-        
-        var socket = io.connect('http://10.74.17.233:1150');
-
         $scope.roomId = $stateParams.roomId
         $scope.empId = '484074'; //AuthService.username() **Hard-Code**
         $scope.message = '';
@@ -68,23 +50,80 @@ angular.module('starter')
         $scope.allSubscribe = [];
         $scope.scrollDetails = {start:0,retrieve:10};
         var viewScroll = $ionicScrollDelegate.$getByHandle('userMessageScroll');
+        var socket;
 
-        //Set roomName
-        SetRoomName($scope,PMRoomSQLite);
-
-        //sync msg and insert/update into sqlite (post to api number 34 to bulk update and get last msg data)
+        //popover private message menus
+        $ionicPopover.fromTemplateUrl('templates/PMMenus.html', {scope: $scope,}).then(function(popover) { $scope.popover = popover; });
         APIService.ShowLoading();
-        SyncService.SyncPMMsg($scope.roomId).then(
-            function(response){
-                var listMsgChanged = (response != null && response.length > 0) ? response : [];
-                //loop list to trigger web socket reenter_received_message
-                IterationSendReEnterEvent(listMsgChanged,socket,$scope.roomId);
-                //sync subscribe
-                SyncService.SyncSubscribe($scope.roomId).then(function(){
-                    //select all message from sqlite and bind to msgDetails
-                    PMMsgInitialProcess(PMMsgSQLite,PMSubscribeSQLite,$scope,APIService,viewScroll,$filter);
+
+        $scope.noInternet = false;
+        //if no internet connection
+        if(!CheckNetwork($cordovaNetwork)){
+            $scope.noInternet = true;
+            OpenIonicAlertPopup($ionicPopup,'ไม่มีสัญญานอินเตอร์เนท','ไม่สามารถใช้งานส่วนนี้ได้เนื่องจากไม่ได้เชื่อมต่ออินเตอร์เนท');
+            //select all message from sqlite and bind to msgDetails
+            PMMsgInitialProcess(PMMsgSQLite,PMSubscribeSQLite,$scope,APIService,viewScroll,$filter,PMRoomSQLite);
+        }
+        else{
+            socket = io.connect('http://10.74.17.233:1150');
+
+            //join to current room
+            socket.emit('join_room',$scope.roomId);
+
+            //sync msg and insert/update into sqlite (post to api number 34 to bulk update and get last msg data)
+            SyncService.SyncPMMsg($scope.roomId).then(
+                function(response){
+                    var listMsgChanged = (response != null && response.length > 0) ? response : [];
+                    //loop list to trigger web socket reenter_received_message
+                    IterationSendReEnterEvent(listMsgChanged,socket,$scope.roomId);
+                    //sync subscribe
+                    SyncService.SyncSubscribe($scope.roomId).then(function(){
+                        //select all message from sqlite and bind to msgDetails
+                        PMMsgInitialProcess(PMMsgSQLite,PMSubscribeSQLite,$scope,APIService,viewScroll,$filter,PMRoomSQLite);
+                    });
+            });
+
+            //push msg from sender
+            socket.on('append_message',function(msg){
+                var data = {Id:msg.Id,Empl_Code:$scope.empId,message:msg.msg,readTotal:0,roomId:$scope.roomId,DL:false,TS:msg.TS};
+                msg.TS = TransformServerTSToDateTimeStr(msg.TS.toString());
+                //insert msg into sqlite
+                PMMsgSQLite.Add([data],false).then(function(){
+                    //push msg
+                    $scope.msgDetails.push(msg);
+                    viewScroll.scrollBottom(true);
+                    //web socket send back to update readed flag
+                    socket.emit('received_message',$scope.roomId,$scope.empId,msg.Id);    
                 });
-        });
+            });
+
+            //append msg to sender side when insert DB success
+            socket.on('send_success',function(retFromServer,recvMSG){
+                var data = {Id:retFromServer.Id,Empl_Code:$scope.empId,message:retFromServer.msg,readTotal:0,roomId:$scope.roomId,DL:false,TS:retFromServer.TS};
+                //save to sqlite
+                PMMsgSQLite.Add([data],false).then(function(){
+                    //find msg in msgDetails by tmpId and edit TS,Id
+                    UpdateSendDateTimeToMsg($scope.msgDetails,retFromServer);
+                    $scope.$apply();
+                    viewScroll.scrollBottom(true);
+                    //web socket send to receiver for append message
+                    socket.emit('append_message_to_receiver',$scope.roomId,recvMSG);
+                });
+            });
+
+            socket.on('set_readTotal',function(msgId,readTotal){
+                if($scope.msgDetails == null || $scope.msgDetails.length <= 0) return;
+                //set readeTotal use filter by msgid
+                for (var i = 0; i <= $scope.msgDetails.length - 1; i++) {
+                    if(parseInt($scope.msgDetails[i].Id) == parseInt(msgId)){
+                        $scope.msgDetails[i].readTotal = readTotal;
+                        break;
+                    }
+                };
+                $scope.$apply();
+            });
+
+        }
 
         $scope.loadPreviousMsg = function(){
             if($scope.scrollDetails.start == 0) return FinalAction($scope,APIService);
@@ -95,17 +134,12 @@ angular.module('starter')
             }
             else $scope.scrollDetails.start = ($scope.scrollDetails.start - $scope.scrollDetails.retrieve);
             var result = GetInfiniteScrollDataReverse($scope.allMsg,$scope.scrollDetails.start,$scope.scrollDetails.retrieve);
-            InitialPMMsgDetails($scope,result,$scope.empId);
+            InitialPMMsgDetails($scope,result,$scope.empId,$scope.allSubscribe,$filter);
             FinalAction($scope,APIService);
         };
 
-        // //sync subscribe and check this room is group/1:1
-        // SyncSubscribe($scope,APIService,$scope.roomId,PMSubscribeSQLite);
-
-        //join to current room
-        socket.emit('join_room',$scope.roomId);
-
         $scope.sendMessage = function(){
+            if($scope.noInternet) return;
             //gen tmpid
             var tmpId = GeneratedGUID();
             //web socket send msg
@@ -117,50 +151,9 @@ angular.module('starter')
             viewScroll.scrollBottom(true);
         };
 
-        //push msg from sender
-        socket.on('append_message',function(msg){
-            console.log('append_message');
-            var data = {Id:msg.Id,Empl_Code:$scope.empId,message:msg.msg,readTotal:0,roomId:$scope.roomId,DL:false,TS:msg.TS};
-            msg.TS = TransformServerTSToDateTimeStr(msg.TS.toString());
-            //insert msg into sqlite
-            PMMsgSQLite.Add([data],false).then(function(){
-                //push msg
-                $scope.msgDetails.push(msg);
-                viewScroll.scrollBottom(true);
-                //web socket send back to update readed flag
-                socket.emit('received_message',$scope.roomId,$scope.empId,msg.Id);    
-            });
-        });
-
-        //append msg to sender side when insert DB success
-        socket.on('send_success',function(retFromServer,recvMSG){
-            var data = {Id:retFromServer.Id,Empl_Code:$scope.empId,message:retFromServer.msg,readTotal:0,roomId:$scope.roomId,DL:false,TS:retFromServer.TS};
-            //save to sqlite
-            PMMsgSQLite.Add([data],false).then(function(){
-                //find msg in msgDetails by tmpId and edit TS,Id
-                UpdateSendDateTimeToMsg($scope.msgDetails,retFromServer);
-                $scope.$apply();
-                viewScroll.scrollBottom(true);
-                //web socket send to receiver for append message
-                socket.emit('append_message_to_receiver',$scope.roomId,recvMSG);
-            });
-        });
-
-        socket.on('set_readTotal',function(msgId,readTotal){
-            if($scope.msgDetails == null || $scope.msgDetails.length <= 0) return;
-            //set readeTotal use filter by msgid
-            for (var i = 0; i <= $scope.msgDetails.length - 1; i++) {
-                if(parseInt($scope.msgDetails[i].Id) == parseInt(msgId)){
-                    $scope.msgDetails[i].readTotal = readTotal;
-                    break;
-                }
-            };
-            $scope.$apply();
-        });
-
         $rootScope.$on( "$stateChangeSuccess", function(e, toState, toParams, fromState, fromParams) {
             if(fromState.url = '/pmsmsgs/:roomId'){
-                socket.disconnect();
+               if(!$scope.noInternet) socket.disconnect();
             }
         });
 
@@ -191,47 +184,6 @@ function GetInfiniteScrollDataReverse(alldata,start,retrieve){
     };
     return result;
 };
-
-// //******************************************************
-// function SyncSubscribe($scope,APIService,roomId,PMSubscribeSQLite){
-    
-//     var url = APIService.hostname() + '/PM/GetEmpInRoom';
-//     APIService.httpPost(url,{roomID:roomId},function(response){
-//         if(response.data != null) {
-//             if(response.data.length > 2) $scope.isGroup = true;
-//             //sync subscribe
-//             var directoryURL = APIService.hostname() + '/ContactDirectory/viewContactPaging';
-//             angular.forEach(response.data,function(value,key){
-//                 PMSubscribeSQLite.CountSubscribeByEmpId(value.Empl_Code).then(
-//                     function(response){
-//                         if(response != null && response.rows != null){
-//                             var result = ConvertQueryResultToArray(response);
-//                             if(result[0].totalCount == 0){
-//                                 //get info & insert into pmsubsribe
-//                                 var directoryData = {keyword:value.Empl_Code,start:1,retrieve:1};
-//                                 APIService.httpPost(directoryURL,directoryData,
-//                                     function(response){
-//                                         var result = response.data[0];
-//                                         if(response != null && response.data != null){
-//                                             //convert picthumb to base64
-//                                             ConvertImgPNGToBase64(result.PictureThumb,function(base64){
-//                                                 if(base64 && base64.length > 0){
-//                                                     //save user data to pmsubscribe
-//                                                     var data = {Empl_Code:result.UserID,Firstname:result.Firstname,Lastname:result.Lastname,PictureThumb:base64};
-//                                                     PMSubscribeSQLite.Add([data]);
-//                                                 }
-//                                             });
-//                                         };
-//                                     },
-//                                     function(error){console.log(error);});
-//                             }
-//                         };
-//                     });
-//             });
-//         }
-//     },function(){});
-// };
-// //******************************************************
 
 function IterationSendReEnterEvent(listChanged,socket,roomId){
     angular.forEach(listChanged,function(value,key){
@@ -276,7 +228,10 @@ function SetRoomName($scope,PMRoomSQLite){
     });
 };
 
-function PMMsgInitialProcess(PMMsgSQLite,PMSubscribeSQLite,$scope,APIService,viewScroll,$filter){
+function PMMsgInitialProcess(PMMsgSQLite,PMSubscribeSQLite,$scope,APIService,viewScroll,$filter,PMRoomSQLite){
+    //Set roomName
+    SetRoomName($scope,PMRoomSQLite);
+
     APIService.ShowLoading();
     PMSubscribeSQLite.GetAllSubScribe().then(function(response){
         if(response != null && response.rows != null){
@@ -288,6 +243,8 @@ function PMMsgInitialProcess(PMMsgSQLite,PMSubscribeSQLite,$scope,APIService,vie
                     var result = GetInfiniteScrollDataReverse($scope.allMsg,$scope.scrollDetails.start,$scope.scrollDetails.retrieve);
                     InitialPMMsgDetails($scope,result,$scope.empId,$scope.allSubscribe,$filter);
                     viewScroll.scrollBottom(true);
+                    //update totalNewMsg = 0 this room because readed all messages in this room.
+                    PMRoomSQLite.UpdateReadAllMsg($scope.roomId);
                 }
                 APIService.HideLoading();
             });    
@@ -305,4 +262,17 @@ function PMMsgInitialProcess(PMMsgSQLite,PMSubscribeSQLite,$scope,APIService,vie
             }
         },
         function(error){});
+};
+
+function PMRoomInitialProcess($scope,PMRoomSQLite,APIService){
+    //select all rooms from sqlite and bind to roomsDetail        
+    PMRoomSQLite.GetAll().then(
+        function(response){
+            if(response.rows != null) {
+                $scope.allRooms = ConvertQueryResultToArray(response);
+                $scope.roomsDetail = GetInfiniteScrollData($scope.allRooms,$scope.scrollDetails.start,$scope.scrollDetails.retrieve);
+                $scope.haveMoreData = (($scope.scrollDetails.start + $scope.scrollDetails.retrieve) < $scope.allRooms.length) ? true : false;
+                FinalAction($scope, APIService);
+            }
+    },function(){FinalAction($scope, APIService);});
 };
